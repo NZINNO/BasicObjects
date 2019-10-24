15d997f525d6777505242a797d317c3bfc768509
Zinno <4070806@vtsd.com>
2019-10-24 08:03:49 -0400

    CH6 Notes

--- Car.java
+++ Car.java
@@ -1,6 +1,22 @@
 public class Car{
+  //Attributes
+  int wheels;
+  String engine;
 
+  // Default Constructor
+  public Car(){
+    wheels = 4;
+    engine = "no idea";
+  }
 
+  public Car(int numWheels, String myEngine){
+    wheels = numWheels;
+    engine = myEngine;
+  }
 
+  // Getter Method
+  public int getWheels(){
+    return wheels;
+  }
   
 }
--- Runner.java
+++ Runner.java
@@ -1,6 +1,9 @@
 public class Runner{
   public static void main(String args[]){
+    Car Nick = new Car();
+    Car Mike = new Car(4, "v6");
 
-    
+    System.out.println(Nick.getWheels() + " " + Nick.engine);
+    System.out.println(Mike.getWheels() + " " + Mike.engine);
   }
 }
